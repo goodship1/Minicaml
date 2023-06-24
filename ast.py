00a941fa339c0f32b619f7b843793ae890f60d13
Gavin Goodship <msgoodship1@gmail.com>
2023-06-24 22:32:43 +0100

    Update ast.py

--- ast.py
+++ ast.py
@@ -1,1 +1,24 @@
+class ProgramNode:
+    def __init__(self, statements):
+        self.statements = statements
+
+class VariableDeclarationNode:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+class AdditionNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class SubtractionNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class MultiplicationNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
 
