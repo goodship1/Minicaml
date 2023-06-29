4cbeca2913f06fc60b4483020f508db7eeaec38c
Gavin Goodship <msgoodship1@gmail.com>
2023-06-29 23:33:56 +0100

    Update funcutiontable.py

--- funcutiontable.py
+++ funcutiontable.py
@@ -1,1 +1,20 @@
+class FunctionSymbolTable:
+    def __init__(self):
+        self.functions = {}
+
+    def add_function(self, name, parameters, return_type):
+        if name in self.functions:
+            raise Exception("already defined")
+        self.functions[name] = {
+            'name': name,
+            'parameters': parameters,
+            'return_type': return_type
+        }
+
+    def get_function(self, name):
+        if name not in self.functions:
+            raise Exception("Function  not found")
+        return self.functions[name]
+
+
 
